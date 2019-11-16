d1f54f056ba691f58f39a953bbb186de4bfca13c
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-16 15:57:16 -0800

    slight modification made to book viewer

---
+++ book_viewer.rb
@@ -0,0 +1,59 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "tilt/erubis"
+
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+not_found do # adding some code in 
+  redirect "/"
+end
+
+def each_chapter
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+def chapters_matching(query)
+  results = []
+
+  return results if !query || query.empty?
+
+  each_chapter do |number, name, contents|
+    results << {number: number, name: name} if contents.include?(query)
+  end
+
+  results
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").each_with_index.map do |line, index|
+      "<p id=paragraph#{index}>#{line}</p>"
+    end.join
+  end
+end
+
+get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+
+  erb :home
+end
+
+get "/chapters/:number" do
+  @num = params[:number].to_i
+  chapter_name = @contents[@num - 1]
+  @title = "Chapter #{@num}: #{chapter_name}"
+  @chapter = File.read("data/chp#{@num}.txt")
+
+  erb :chapter
+end
+
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb :search
+end
